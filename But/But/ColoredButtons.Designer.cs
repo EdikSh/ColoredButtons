87a37d69aa4f92a202eecc5b4e3239cf981ad2f4
EdikSh <93342333+EdikSh@users.noreply.github.com>
2021-12-13 16:46:37 +0200

    Add files via upload

---
+++ But/But/ColoredButtons.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace But
+{
+    partial class ColoredButtons
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bNavy = new System.Windows.Forms.Button();
+            this.bBlue = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.bAqua = new System.Windows.Forms.Button();
+            this.bTeal = new System.Windows.Forms.Button();
+            this.bOlive = new System.Windows.Forms.Button();
+            this.bGreen = new System.Windows.Forms.Button();
+            this.bLime = new System.Windows.Forms.Button();
+            this.bYellow = new System.Windows.Forms.Button();
+            this.bOrange = new System.Windows.Forms.Button();
+            this.bRed = new System.Windows.Forms.Button();
+            this.bMaroon = new System.Windows.Forms.Button();
+            this.bFuchsia = new System.Windows.Forms.Button();
+            this.bPurple = new System.Windows.Forms.Button();
+            this.bBlack = new System.Windows.Forms.Button();
+            this.bSilver = new System.Windows.Forms.Button();
+            this.bGray = new System.Windows.Forms.Button();
+            this.bWhite = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bNavy
+            // 
+            this.bNavy.ForeColor = System.Drawing.Color.Navy;
+            this.bNavy.Location = new System.Drawing.Point(3, 3);
+            this.bNavy.Name = "bNavy";
+            this.bNavy.Size = new System.Drawing.Size(75, 23);
+            this.bNavy.TabIndex = 0;
+            this.bNavy.Text = "Navy";
+            this.bNavy.UseVisualStyleBackColor = true;
+            // 
+            // bBlue
+            // 
+            this.bBlue.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.bBlue.ForeColor = System.Drawing.Color.Blue;
+            this.bBlue.Location = new System.Drawing.Point(84, 3);
+            this.bBlue.Name = "bBlue";
+            this.bBlue.Size = new System.Drawing.Size(75, 23);
+            this.bBlue.TabIndex = 1;
+            this.bBlue.Text = "Blue";
+            this.bBlue.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Controls.Add(this.bNavy);
+            this.flowLayoutPanel1.Controls.Add(this.bBlue);
+            this.flowLayoutPanel1.Controls.Add(this.bAqua);
+            this.flowLayoutPanel1.Controls.Add(this.bTeal);
+            this.flowLayoutPanel1.Controls.Add(this.bOlive);
+            this.flowLayoutPanel1.Controls.Add(this.bGreen);
+            this.flowLayoutPanel1.Controls.Add(this.bLime);
+            this.flowLayoutPanel1.Controls.Add(this.bYellow);
+            this.flowLayoutPanel1.Controls.Add(this.bOrange);
+            this.flowLayoutPanel1.Controls.Add(this.bRed);
+            this.flowLayoutPanel1.Controls.Add(this.bMaroon);
+            this.flowLayoutPanel1.Controls.Add(this.bFuchsia);
+            this.flowLayoutPanel1.Controls.Add(this.bPurple);
+            this.flowLayoutPanel1.Controls.Add(this.bBlack);
+            this.flowLayoutPanel1.Controls.Add(this.bSilver);
+            this.flowLayoutPanel1.Controls.Add(this.bGray);
+            this.flowLayoutPanel1.Controls.Add(this.bWhite);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 1);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(491, 110);
+            this.flowLayoutPanel1.TabIndex = 2;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // bAqua
+            // 
+            this.bAqua.ForeColor = System.Drawing.Color.Aqua;
+            this.bAqua.Location = new System.Drawing.Point(165, 3);
+            this.bAqua.Name = "bAqua";
+            this.bAqua.Size = new System.Drawing.Size(75, 23);
+            this.bAqua.TabIndex = 2;
+            this.bAqua.Text = "Aqua";
+            this.bAqua.UseVisualStyleBackColor = true;
+            // 
+            // bTeal
+            // 
+            this.bTeal.ForeColor = System.Drawing.Color.Teal;
+            this.bTeal.Location = new System.Drawing.Point(246, 3);
+            this.bTeal.Name = "bTeal";
+            this.bTeal.Size = new System.Drawing.Size(75, 23);
+            this.bTeal.TabIndex = 3;
+            this.bTeal.Text = "Teal";
+            this.bTeal.UseVisualStyleBackColor = true;
+            // 
+            // bOlive
+            // 
+            this.bOlive.ForeColor = System.Drawing.Color.Olive;
+            this.bOlive.Location = new System.Drawing.Point(327, 3);
+            this.bOlive.Name = "bOlive";
+            this.bOlive.Size = new System.Drawing.Size(75, 23);
+            this.bOlive.TabIndex = 4;
+            this.bOlive.Text = "Olive";
+            this.bOlive.UseVisualStyleBackColor = true;
+            // 
+            // bGreen
+            // 
+            this.bGreen.ForeColor = System.Drawing.Color.Green;
+            this.bGreen.Location = new System.Drawing.Point(408, 3);
+            this.bGreen.Name = "bGreen";
+            this.bGreen.Size = new System.Drawing.Size(75, 23);
+            this.bGreen.TabIndex = 5;
+            this.bGreen.Text = "Green";
+            this.bGreen.UseVisualStyleBackColor = true;
+            // 
+            // bLime
+            // 
+            this.bLime.ForeColor = System.Drawing.Color.Lime;
+            this.bLime.Location = new System.Drawing.Point(3, 32);
+            this.bLime.Name = "bLime";
+            this.bLime.Size = new System.Drawing.Size(75, 23);
+            this.bLime.TabIndex = 6;
+            this.bLime.Text = "Lime";
+            this.bLime.UseVisualStyleBackColor = true;
+            // 
+            // bYellow
+            // 
+            this.bYellow.ForeColor = System.Drawing.Color.Yellow;
+            this.bYellow.Location = new System.Drawing.Point(84, 32);
+            this.bYellow.Name = "bYellow";
+            this.bYellow.Size = new System.Drawing.Size(75, 23);
+            this.bYellow.TabIndex = 7;
+            this.bYellow.Text = "Yellow";
+            this.bYellow.UseVisualStyleBackColor = true;
+            // 
+            // bOrange
+            // 
+            this.bOrange.ForeColor = System.Drawing.Color.Orange;
+            this.bOrange.Location = new System.Drawing.Point(165, 32);
+            this.bOrange.Name = "bOrange";
+            this.bOrange.Size = new System.Drawing.Size(75, 23);
+            this.bOrange.TabIndex = 8;
+            this.bOrange.Text = "Orange";
+            this.bOrange.UseVisualStyleBackColor = true;
+            // 
+            // bRed
+            // 
+            this.bRed.ForeColor = System.Drawing.Color.Red;
+            this.bRed.Location = new System.Drawing.Point(246, 32);
+            this.bRed.Name = "bRed";
+            this.bRed.Size = new System.Drawing.Size(75, 23);
+            this.bRed.TabIndex = 9;
+            this.bRed.Text = "Red";
+            this.bRed.UseVisualStyleBackColor = true;
+            // 
+            // bMaroon
+            // 
+            this.bMaroon.ForeColor = System.Drawing.Color.Maroon;
+            this.bMaroon.Location = new System.Drawing.Point(327, 32);
+            this.bMaroon.Name = "bMaroon";
+            this.bMaroon.Size = new System.Drawing.Size(75, 23);
+            this.bMaroon.TabIndex = 10;
+            this.bMaroon.Text = "Maroon";
+            this.bMaroon.UseVisualStyleBackColor = true;
+            // 
+            // bFuchsia
+            // 
+            this.bFuchsia.ForeColor = System.Drawing.Color.Fuchsia;
+            this.bFuchsia.Location = new System.Drawing.Point(408, 32);
+            this.bFuchsia.Name = "bFuchsia";
+            this.bFuchsia.Size = new System.Drawing.Size(75, 23);
+            this.bFuchsia.TabIndex = 11;
+            this.bFuchsia.Text = "Fuchsia";
+            this.bFuchsia.UseVisualStyleBackColor = true;
+            // 
+            // bPurple
+            // 
+            this.bPurple.ForeColor = System.Drawing.Color.Purple;
+            this.bPurple.Location = new System.Drawing.Point(3, 61);
+            this.bPurple.Name = "bPurple";
+            this.bPurple.Size = new System.Drawing.Size(75, 23);
+            this.bPurple.TabIndex = 12;
+            this.bPurple.Text = "Purple";
+            this.bPurple.UseVisualStyleBackColor = true;
+            // 
+            // bBlack
+            // 
+            this.bBlack.ForeColor = System.Drawing.Color.Black;
+            this.bBlack.Location = new System.Drawing.Point(84, 61);
+            this.bBlack.Name = "bBlack";
+            this.bBlack.Size = new System.Drawing.Size(75, 23);
+            this.bBlack.TabIndex = 13;
+            this.bBlack.Text = "Black";
+            this.bBlack.UseVisualStyleBackColor = true;
+            // 
+            // bSilver
+            // 
+            this.bSilver.ForeColor = System.Drawing.Color.Silver;
+            this.bSilver.Location = new System.Drawing.Point(165, 61);
+            this.bSilver.Name = "bSilver";
+            this.bSilver.Size = new System.Drawing.Size(75, 23);
+            this.bSilver.TabIndex = 14;
+            this.bSilver.Text = "Silver";
+            this.bSilver.UseVisualStyleBackColor = true;
+            // 
+            // bGray
+            // 
+            this.bGray.ForeColor = System.Drawing.Color.Gray;
+            this.bGray.Location = new System.Drawing.Point(246, 61);
+            this.bGray.Name = "bGray";
+            this.bGray.Size = new System.Drawing.Size(75, 23);
+            this.bGray.TabIndex = 15;
+            this.bGray.Text = "Gray";
+            this.bGray.UseVisualStyleBackColor = true;
+            // 
+            // bWhite
+            // 
+            this.bWhite.ForeColor = System.Drawing.Color.White;
+            this.bWhite.Location = new System.Drawing.Point(327, 61);
+            this.bWhite.Name = "bWhite";
+            this.bWhite.Size = new System.Drawing.Size(75, 23);
+            this.bWhite.TabIndex = 16;
+            this.bWhite.Text = "White";
+            this.bWhite.UseVisualStyleBackColor = true;
+            // 
+            // ColoredButtons
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 123);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "ColoredButtons";
+            this.Text = "ColoredButtons";
+            this.Load += new System.EventHandler(this.ColoredButtons_Load);
+            this.Resize += new System.EventHandler(this.ColoredButtons_Resize);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button bNavy;
+        private Button bBlue;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button bAqua;
+        private Button bTeal;
+        private Button bOlive;
+        private Button bGreen;
+        private Button bLime;
+        private Button bYellow;
+        private Button bOrange;
+        private Button bRed;
+        private Button bMaroon;
+        private Button bFuchsia;
+        private Button bPurple;
+        private Button bBlack;
+        private Button bSilver;
+        private Button bGray;
+        private Button bWhite;
+    }
+}
---
+++ But/But/ColoredButtons.cs
@@ -0,0 +1,25 @@
+﻿namespace But
+{
+    public partial class ColoredButtons : Form
+    {
+        public ColoredButtons()
+        {
+            InitializeComponent();
+        }
+
+        private void ColoredButtons_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ColoredButtons_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
